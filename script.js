b2d40844ef849b23b33aca5baa473badbbd766d6
chiouya <chiouyazo@gmail.com>
2024-07-12 11:18:22 +0200

    script test

--- script.js
+++ script.js
@@ -17,3 +17,38 @@ function GetTitle(blogName, target) {
         .then(data => document.getElementById(target).innerHTML = data)
         .catch(error => console.error('Error fetching data:', error));
 }
+
+MakeBigBlogDisplay('00_pricing', '00_pricing');
+
+function MakeBigBlogDisplay(blogName, target) {
+    const anchor = document.createElement('a');
+    anchor.className = 'stretchedContent';
+    anchor.href = 'blogs/' + blogName + '/index.html';
+
+    const div1 = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = 'blogs/' + blogName + '/resources/preview.png';
+    div1.appendChild(img);
+
+    const div2 = document.createElement('div');
+
+    const div3 = document.createElement('div');
+    div3.id = blogName + '_Title';
+
+    const div4 = document.createElement('div');
+    const div5 = document.createElement('div');
+    div5.id = blogName + '_Description';
+    div4.appendChild(div5);
+
+    div2.appendChild(div3);
+    div2.appendChild(div4);
+
+    anchor.appendChild(div1);
+    anchor.appendChild(div2);
+
+    const targetDiv = document.getElementById(target);
+    targetDiv.appendChild(anchor);
+
+    GetTitle('00_pricing', '00_pricing_Title');
+    GetDescription('00_pricing', '00_pricing_Description');
+}
